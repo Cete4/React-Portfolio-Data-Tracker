c6e5ef15316ed7760d0a4fa663c676e7c5a5f064
Cete4 <46322113+Cete4@users.noreply.github.com>
2022-04-27 14:16:27 -0500

    Update

---
+++ src/main/kotlin/com/example/reactportfoliodatatracker/ReactPortfolioDataTrackerApplication.kt
@@ -0,0 +1,13 @@
+package com.example.reactportfoliodatatracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.SpringApplication
+
+@SpringBootApplication
+open class ReactPortfolioDataTracker {
+
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ReactPortfolioDataTracker::class.java, *args)
+}
---
+++ src/main/kotlin/com/example/reactportfoliodatatracker/controller/WebsiteView.kt
@@ -0,0 +1,16 @@
+package com.example.reactportfoliodatatracker.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/api")
+@RestController
+class WebsiteView {
+
+    @GetMapping(value = ["/view"])
+    fun getViewInfo(): String {
+
+        return "Success"
+    }
+}
